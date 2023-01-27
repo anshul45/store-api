1751b665579d1e1f052a47b5d8c19468b4d2654c
anshul45 <80778090+anshul45@users.noreply.github.com>
2023-01-27 21:08:02 +0530

    created limit skip in api's

--- controllers/products.js
+++ controllers/products.js
@@ -2,12 +2,15 @@ const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
   const search = "ab";
-  const products = await Product.find({}).sort("-name price");
+  const products = await Product.find({})
+    .sort("name")
+    .select("name price")
+    .limit(10);
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -21,12 +24,25 @@ const getAllProducts = async (req, res) => {
 
   // console.log(queryObject);
   let result = Product.find(queryObject);
+  // sort
   if (sort) {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
     result = result.sort("createdAt");
   }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+  //
+
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
